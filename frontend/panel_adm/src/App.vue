bb3415a9cdadfe5972eb824b4867d59578a55955
Celian369 <celian.cusey@epita.fr>
2024-04-18 18:00:16 +0200

    feat(init): panel adm initiated with primevue, tailwind and typescript

---
+++ frontend/panel_adm/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import SideMenu from "@/components/SideMenu.vue";
+import TheHeader from "@/components/TheHeader.vue";
+import MainPanel from "@/components/MainPanel.vue";
+</script>
+
+<template>
+  <TheHeader/>
+  <main class="h-full flex flex-wrap">
+    <SideMenu/>
+    <MainPanel/>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
