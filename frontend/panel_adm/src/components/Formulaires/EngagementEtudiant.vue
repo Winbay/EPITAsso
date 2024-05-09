58383fcddfefe04815f46723696ff93d3faebd6f
Nathan Lenogue <nathan.lenogue@epita.fr>
2024-05-09 13:08:53 +0200

    feat: init component

---
+++ frontend/panel_adm/src/components/Formulaires/EngagementEtudiant.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Engagement étudiant</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/panel_adm/src/router/index.ts
+++ frontend/panel_adm/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "../components/Home.vue";
+import EngagementEtudiant from '@/components/Formulaires/EngagementEtudiant.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home},
+    { path:'/engagement-etudiant', component: EngagementEtudiant}
   ]
 })
 
