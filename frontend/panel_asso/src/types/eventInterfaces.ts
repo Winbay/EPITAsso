de264e0e13f7435b7bc748045d3fb1b333fd0065
GitHub Action CI <Skraam@users.noreply.github.com>
2024-05-11 23:49:09 +0000

    ci: Format and lint code

--- frontend/panel_asso/src/types/eventInterfaces.ts
+++ frontend/panel_asso/src/types/eventInterfaces.ts
@@ -1,14 +1,14 @@
 export interface EventModification {
-  id: number;
-  title: string;
-  content: string;
-  author: string;
-  tags: number[];
-  startDate: number;
-  endDate: number;
-  recurrent: boolean;
-  frequency: number;
-  endRecurrence: number;
+  id: number
+  title: string
+  content: string
+  author: string
+  tags: number[]
+  startDate: number
+  endDate: number
+  recurrent: boolean
+  frequency: number
+  endRecurrence: number
 }
 
 export interface EventCreation {
