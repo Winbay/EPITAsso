b4a99c51ddd27d24ebb4bdb35b91d7ff05e42e07
GitHub Action CI <Skraam@users.noreply.github.com>
2024-05-12 00:05:12 +0000

    ci: Format and lint code

--- frontend/panel_adm/src/components/__tests__/empty.spec.ts
+++ frontend/panel_adm/src/components/__tests__/empty.spec.ts
@@ -1,12 +1,12 @@
 // File to remove, juste here to not block tests run in CI
 
-import { shallowMount } from '@vue/test-utils';
-import MainPanel from '@/components/MainPanel.vue'; // Assurez-vous de remplacer 'MyComponent' par le nom réel de votre composant
-import { describe, it, expect } from 'vitest';
+import { shallowMount } from '@vue/test-utils'
+import MainPanel from '@/components/MainPanel.vue' // Assurez-vous de remplacer 'MyComponent' par le nom réel de votre composant
+import { describe, it, expect } from 'vitest'
 
 describe('MainPanel', () => {
   it('always returns true', () => {
-    const wrapper = shallowMount(MainPanel);
-    expect(true).toBe(true);
-  });
-});
+    const wrapper = shallowMount(MainPanel)
+    expect(true).toBe(true)
+  })
+})
