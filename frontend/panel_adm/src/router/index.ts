5c907f1ab3884cb54a8a8d8d69c4293ab418e105
Celian369 <celian.cusey@epita.fr>
2024-05-14 18:48:51 +0200

    J'aime pas les CI

--- frontend/panel_adm/src/router/index.ts
+++ frontend/panel_adm/src/router/index.ts
@@ -7,8 +7,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
       { path: '/', component: Home },
-      { path: '/associations', component: Association }
-    { path: '/engagement-etudiant', component: StudentEngagement }
+      { path: '/associations', component: Association },
+        { path: '/engagement-etudiant', component: StudentEngagement }
   ]
 })
 
