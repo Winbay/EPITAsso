3e7188fe064b9f0971c5cf4a8f2b2737b9dd669c
GitHub Action CI <Winbay@users.noreply.github.com>
2024-05-31 16:23:41 +0000

    ci: Format and lint code

--- backend/EPITAssoAPI/user/admin.py
+++ backend/EPITAssoAPI/user/admin.py
@@ -2,16 +2,48 @@ from django.contrib import admin
 from .models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'login', 'school', 'microsoft_id')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_custom_admin', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "login",
+                    "school",
+                    "microsoft_id",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_custom_admin",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     list_display = (
-        'email', 'login', 'first_name', 'last_name', 'school', 'is_custom_admin', 'is_staff', 'is_superuser'
+        "email",
+        "login",
+        "first_name",
+        "last_name",
+        "school",
+        "is_custom_admin",
+        "is_staff",
+        "is_superuser",
     )
-    search_fields = ('login', 'email', 'first_name', 'last_name', 'school')
-    list_filter = ('is_custom_admin', 'is_staff', 'is_superuser', 'school')
+    search_fields = ("login", "email", "first_name", "last_name", "school")
+    list_filter = ("is_custom_admin", "is_staff", "is_superuser", "school")
