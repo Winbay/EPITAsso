0e432773ed6dc4c82d7d2b03614092288fffe86c
Nathan Lenogue <nathan.lenogue@epita.fr>
2024-09-10 17:52:39 +0200

    fix: resolve build issues

--- frontend/panel_asso/src/components/Messaging/MessagesView.vue
+++ frontend/panel_asso/src/components/Messaging/MessagesView.vue
@@ -247,7 +247,7 @@ const handleWebSocketMessage = (event: MessageEvent): void => {
 
   if (data.type === 'message_sent') {
     const message = messageService.transformMessageFromWS(messageData)
-    if (message.author.login !== userStore.getUser.login || message.associationSender.id !== selectedAssociation.value.id) {
+    if (message.author.login !== user.value.login || message.associationSender.id !== selectedAssociation.value.id) {
       messagesRef.value.push(message)
       scrollToEnd()
     }
